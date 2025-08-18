6043f86597fcdb5a4f18c773ffc1ad1da16bf613
Anthony Bouch <anthony@infonomic.io>
2025-08-19 01:01:38 +0700

    feat: main page removed unnessary div and overflow setting

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -8,36 +8,34 @@ export default async function HomePage() {
   const { installations } = await getRegistry()
   return (
     <Section className="relative mb-12">
-      <div className="w-full overflow-hidden">
-        <Branding />
-        <Container className="relative flex flex-col">
-          <div className="prose">
-            <h1 className="text-center mb-8">Modulus Installations</h1>
-            <p className="m-0 mb-1 ml-[3px]">Loaded {installations.length} installations.</p>
-          </div>
-          <div className="grid grid-cols-auto-fit-320 gap-6">
-            {installations.map((registrant) => (
-              <Card key={registrant.id} hover={false}>
-                <Card.Header>
-                  <Card.Title>{registrant['site-name']}</Card.Title>
-                  <Card.Description>{registrant.organization}</Card.Description>
-                </Card.Header>
-                <Card.Content className="wrap-anywhere">
-                  Visit:{' '}
-                  <a
-                    className="hover:underline"
-                    href={registrant['site-url']}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {registrant['site-url']}
-                  </a>
-                </Card.Content>
-              </Card>
-            ))}
-          </div>
-        </Container>
-      </div>
+      <Branding />
+      <Container className="relative flex flex-col">
+        <div className="prose">
+          <h1 className="text-center mb-8">Modulus Installations</h1>
+          <p className="m-0 mb-1 ml-[3px]">Loaded {installations.length} installations.</p>
+        </div>
+        <div className="grid grid-cols-auto-fit-320 gap-6">
+          {installations.map((registrant) => (
+            <Card key={registrant.id} hover={false}>
+              <Card.Header>
+                <Card.Title>{registrant['site-name']}</Card.Title>
+                <Card.Description>{registrant.organization}</Card.Description>
+              </Card.Header>
+              <Card.Content className="wrap-anywhere">
+                Visit:{' '}
+                <a
+                  className="hover:underline"
+                  href={registrant['site-url']}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {registrant['site-url']}
+                </a>
+              </Card.Content>
+            </Card>
+          ))}
+        </div>
+      </Container>
     </Section>
   )
 }
